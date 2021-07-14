a01306c181468b27b24941a7eb19a0135e45aee1
irumvanselme <andesanselme@gmail.com>
2021-07-14 13:12:59 +0200

    Add roles in the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum("roles", ["CUSTOMER", "ADMIN"])->default("CUSTOMER");
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
